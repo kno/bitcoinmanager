c640c66f5b71170f3b18f558087e9d2fbf7f973c
kno <linsms@gmail.com>
2021-02-26 15:00:11 +0100

    Add sql structure Remove footer links

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -129,15 +129,6 @@ const Home = () => {
       </div>
       <DataGrid autoHeight rows={trades.concat(totals)} columns={columns} />
       <ul className="Home-resources">
-        <li>
-          <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-        </li>
-        <li>
-          <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-        </li>
-        <li>
-          <a href="https://palmer.chat">Community Slack</a>
-        </li>
       </ul>
     </div>
   );
