f865bd795daac875bf50aa0e7e9bc48aa3d2d111
kno <linsms@gmail.com>
2021-04-13 19:55:35 +0200

    Better display

--- front/src/components/DesktopList.js
+++ front/src/components/DesktopList.js
@@ -1,6 +1,7 @@
 import { DataGrid } from "@material-ui/data-grid";
 import DeleteIcon from "@material-ui/icons/Delete";
 import React, { useContext } from "react";
+import NumberFormat from "react-number-format";
 import { deleteRow } from "../services/trades";
 import { Context } from "../store";
 
@@ -8,18 +9,48 @@ const DesktopList = ({ trades }) => {
   const { state, dispatch } = useContext(Context);
   const { rate, token, password } = state || {};
 
+  const FormatNumber = (num, decimals = 2) => (
+    <NumberFormat
+      value={num}
+      displayType={"text"}
+      decimalScale={decimals}
+      suffix={"€"}
+    />
+  );
+
   const columns = [
     { field: "date", headerName: "Date", width: 130 },
-    { field: "amount", headerName: "Amount", width: 130 },
-    { field: "rate", headerName: "Buy Rate", width: 130 },
-    { field: "coin", headerName: "Coin", width: 130 },
-    { field: "btc", headerName: "BTC", width: 150 },
-    { field: "value", headerName: "Current Value", width: 150 },
-    { field: "benefit", headerName: "Benefit", width: 150 },
+    {
+      field: "amount",
+      headerName: "Amount",
+      width: 100,
+      renderCell: (a) => FormatNumber(a.value),
+    },
+    {
+      field: "rate",
+      headerName: "Buy Rate",
+      width: 100,
+      renderCell: (a) => FormatNumber(a.value),
+    },
+    { field: "coin", headerName: "Coin", width: 100 },
+    { field: "btc", headerName: "BTC", width: 200 },
+    {
+      field: "value",
+      type: "number",
+      headerName: "Current Value",
+      width: 150,
+      renderCell: (a) => FormatNumber(a.value),
+    },
+    {
+      field: "benefit",
+      headerName: "Benefit",
+      width: 100,
+      renderCell: (a) => FormatNumber(a.value),
+    },
     {
       field: "actions",
       headerName: "Actions",
-      width: 150,
+      width: 130,
       renderCell: (a) => {
         return (
           a.row.id && (
